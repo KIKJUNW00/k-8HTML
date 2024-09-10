0eac983b9383d5e1f2c2f17c60ed52b6592aa476
KIKJUNW00 <kimjun9758@naver.com>
2024-09-10 14:01:53 +0900

    js_0910

--- 12/12.js
+++ 12/12.js
@@ -1,26 +1,71 @@
-//데이터 가져오기
-const getData = (ul) => {
-    console.log('getData') ;
-    let url = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=82ca741a2844c5c180a208137bb92bd7&targetDt=20120101';
-  
+//OPEN API 데이터 가져오기
+const getData = (selDt, ul) => {
+    console.log(selDt);
+    const testAPI = '82ca741a2844c5c180a208137bb92bd7';
+    let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
+    url = `${url}key=${testAPI}&targetDt=${selDt}`;
+
     fetch(url)
-      .then( resp => resp.json() )
-      // .then( data => console.log(data.boxOfficeResult['dailyBoxOfficeList']))
-      .then( data => {
-        let dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList ;
+        .then(resp => resp.json())
+        .then(data => {
+            let dailyBoxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
+
+            let tm = dailyBoxOfficeList.map(item => 
+                `<li class='mvli'>
+                    <span class='rank'>${item.rank}</span>
+                    <sapn class='movieNm'>${item.movieNm}</sapn>
+                    <span class='openDt'>${item.openDt}</span>
+                  </li>`)
         
-        let tm = dailyBoxOfficeList.map(item => `<li>${item.movieNm}</li>`);
-        console.log(tm);
-        ul.innerHTML = tm.join('') ;
-        
-      })
-      .catch( err => console.error(err)) ; 
-  } 
-  
-  
-  document.addEventListener('DOMContentLoaded', ()=>{
-    //요소 가져오기
-    const ul = document.querySelector('.sec > ul') ;
-    getData(ul) ;
-  
-  });
+              tm = tm.join('')
+              ul.innerHTML = tm ;
+
+              console.log(tm)
+
+        })
+        .catch(err => console.log(err));
+
+    console.log(url);
+}
+
+//어제 날짜 구하기 함수
+const getYesterday = () => {
+    const yesterday = new Date();
+    // const yesterday = new Date(2024, 8, 12);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const year = yesterday.getFullYear();
+    let month = (yesterday.getMonth() + 1);
+    let day = yesterday.getDate();
+
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+
+    // month = `0${month}`.slice(-2);
+    // month = `${month}`.padStart(2, '0');
+    // day = `0${day}`.slice(-2);
+
+    return `${year}-${month}-${day}`;
+}
+
+//DOM생성후
+document.addEventListener('DOMContentLoaded', () => {
+    // DOM요소 가져오기
+    const dt = document.querySelector('#dt');
+    const ul = document.querySelector('.sec > ul');
+
+
+    //어제 날짜 구하기
+    let yesterday = getYesterday();
+    console.log('yesterday: ', yesterday);
+
+    // data요소 최대값
+    dt.max = yesterday;
+
+    //데이터 가져오기
+    dt.addEventListener('change', (e) => {
+        e.preventDefault();
+        getData(dt.value.replaceAll('-',''), ul);
+    });
+
+});
